8a438b97b3af4220ca75af44d801db4f56d44587
Joel siby <joelsiby@Joels-MacBook-Air.local>
2026-07-09 04:01:33 +0530

    zoom integration new

--- app/(dashboard)/student/profile/StudentProfileClient.tsx
+++ app/(dashboard)/student/profile/StudentProfileClient.tsx
@@ -118,6 +118,7 @@ export default function StudentProfileClient({
     
     setLoadingPhone(true);
     // Triggers SMS OTP to the EXISTING number
+    //Test
     const { error } = await supabase.auth.updateUser({ 
       phone: phoneToVerify 
     });
